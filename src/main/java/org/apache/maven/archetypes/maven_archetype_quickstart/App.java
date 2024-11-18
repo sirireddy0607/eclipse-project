6fb58820791a2f8d346f4b248ffd49eb5aa93922
sirireddy0607 <varsha@gmail.com>
2024-11-18 10:53:29 +0530

    Initial commit for Maven project

---
+++ src/main/java/org/apache/maven/archetypes/maven_archetype_quickstart/App.java
@@ -0,0 +1,10 @@
+package org.apache.maven.archetypes.maven_archetype_quickstart;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
